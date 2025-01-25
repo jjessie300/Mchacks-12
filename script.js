430b6b9d878d08ef90d1f0ce22ffdc75c34926cb
Kiana <kiananyk717@gmail.com>
2025-01-25 15:56:12 -0500

    fetch data when entering username

---
+++ script.js
@@ -0,0 +1,24 @@
+const username = document.getElementById('username');
+const submit_button = document.getElementById('submit');
+
+submit_button.addEventListener("click", () => {
+    const leetcode_username = username.value;
+    
+    // get number of solved problems
+    fetch('http://localhost:3000/' + leetcode_username)
+
+        .then (response => {
+            if (!response.ok){
+                throw new Error('Could not fetch data.');
+            }
+            return response.json();
+        })
+
+        .then(data => {
+            console.log('Data received:', data);
+        })
+
+        .then(error => {
+            console.error('There was an error while fetching: ', error);
+        })
+})
